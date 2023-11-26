6bc55f06f51494b4b2baf3aa4f77cf91059d6d07
lawtlee <lawsac2947@gmail.com>
2023-11-26 02:02:10 -0800

    Update 404 Page

--- src/screens/404html.tsx
+++ src/screens/404html.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const HTML404: React.FC = () => {
-    return(
-        <div className="w-screen h-screen flex justify-center items-center">
-            <img src='404.png' alt="Sorry this page doesn't exist"/>
-        </div>
-    )
+    return (
+        <motion.div
+            className="w-screen h-screen flex justify-center items-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+        >
+            <img src="404.png" alt="Sorry this page doesn't exist" />
+        </motion.div>
+    );
 }
 
 export default HTML404;
