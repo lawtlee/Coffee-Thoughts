9dfbb7f3725481afb3af9068e817ea2a042786ae
lawtlee <lawsac2947@gmail.com>
2023-12-02 16:01:56 -0800

    Update Links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaGithub, FaInstagram } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer:React.FC = () => {
+    const navigate = useNavigate()
+
     return(
         <div className="flex flex-col bottom-0">
             <div className="w-[80vw] border-[#9B8F84] border-[1px]"/>
             <div className="min-w-[80vw] flex flex-row justify-between text-[#9B8F84] h-[7.5vh] items-center">
-                <div className="cursor-default">
+                <div className="cursor-default" onClick={()=>navigate('/admin')}>
                     Coffee Thoughts
                 </div>
                 <div className="flex flex-row gap-2">
